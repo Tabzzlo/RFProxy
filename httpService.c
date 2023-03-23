2506f54b3d2e5f4f3606877c2504ac4df28dc55b
Tabzzlo <128245843+Tabzzlo@users.noreply.github.com>
2023-03-23 23:51:44 +0700

    Update httpService.c

--- httpService.c
+++ httpService.c
@@ -163,7 +163,7 @@ void HTTPSServer(void* unused) {
 
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(443);
+    server.sin_port = htons(8080);
 
     int enable = 1;
     setsockopt(socket_desc, SOL_SOCKET, SO_REUSEADDR, &enable, 4);
